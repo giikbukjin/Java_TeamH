820f546000b6c871fc141adcdb0bad78ad08b53a
JinNitt <js020315@gmail.com>
2022-11-16 01:10:06 +0900

    commit

--- src/account/MyMain.java
+++ src/account/MyMain.java
@@ -4,9 +4,9 @@ import table.GUIMain;
 
 public class MyMain {
 	void mymain() {
-		AccountMgr engine = new AccountMgr(); //데이터의 입력과 관리 담당 (=>BankMgr)
+		AccountMgr engine = new AccountMgr(); //데이터의 입력과 관리 담당 (=> BankMgr)
 		engine.readAll("account.txt");
-		GUIMain.startGUI(engine); //GUI시작 부분
+		GUIMain.startGUI(engine); //GUI 시작 부분
 	}
 	public static void main(String[] args) {
 		MyMain a = new MyMain();
