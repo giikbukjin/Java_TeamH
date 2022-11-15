dcf79ecc107a2d182cff3f7cabff398afd917ac8
JinNitt <js020315@gmail.com>
2022-11-15 21:50:40 +0900

    + login

---
+++ src/login/TestFrame.java
@@ -0,0 +1,13 @@
+package login;
+
+import javax.swing.JFrame;
+
+public class TestFrame extends JFrame{
+   
+	private static final long serialVersionUID = 1L;
+
+	public TestFrame() {
+        setSize(400, 400);
+        setVisible(true);
+    }
+}
