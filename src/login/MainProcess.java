2c1170a4ef60ca74e41779171c4a1aed3c974e04
JinNitt <js020315@gmail.com>
2022-11-16 15:07:09 +0900

    login -> main

--- src/login/MainProcess.java
+++ src/login/MainProcess.java
@@ -1,20 +1,30 @@
 package login;
 
+import swing.MainFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MainProcess{
     LoginView loginView;
-    TestFrame testFrm;
    
     public static void main(String[] args) {
-       
         // 메인클래스 실행
         MainProcess main = new MainProcess();
-        main.loginView = new LoginView(); // 로그인창 보이기
-        main.loginView.setMain(main); // 로그인창에게 메인 클래스보내기
+        main.loginView = new LoginView(); // 로그인 창 보이기
+        main.loginView.setMain(main); // 로그인 창에게 메인 클래스 보내기
     }
    
-    // 테스트프레임창
-    public void showFrameTest(){
-        loginView.dispose(); // 로그인창닫기
-        this.testFrm = new TestFrame(); // 테스트프레임 오픈
+    // 메인프레임 창
+    public void showFrameTest() {
+        loginView.btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //loginView.dispose(); // 로그인 창 닫기
+                loginView.setVisible(false);
+                MainFrame frame = new MainFrame();
+                frame.setVisible(true);
+            }
+        });
     }
 }
