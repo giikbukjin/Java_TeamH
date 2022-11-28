b36c8873b3278f9cfed71db9485ca5df6f4e778d
Shinoyh <tlsgytlr001@kyonggi.ac.kr>
2022-11-28 10:24:05 +0900

    상품추천 추가코드
    상품추천 조건에 따라서 출력되게 했고, 추천 객체 레이아웃설정과 사진 첨부했습니다.

---
+++ src/recommend/RcmdItemMgr.java
@@ -0,0 +1,100 @@
+package recommend;
+
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import facade.DataEngeineInterface2;
+import mgr.Factory;
+import mgr.Manageable;
+import mgr.Manager;
+
+public class RcmdItemMgr extends JPanel {
+	GridBagLayout Gbag = new GridBagLayout();
+	int count = 1;
+	Recommend rec = new Recommend();
+	String cA = rec.kwd1;
+	String aA = rec.kwd2;
+	
+	
+	
+	//currentAsset = kwdTextField1.getText();
+	//ableAsset = kwdTextField2.getText(); 
+	
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RcmdItemMgr() {
+		view();
+	}
+
+	private Manager mgr = new Manager();
+	/*
+	 * public void readAll(String filename) { mgr.readAll("recommend.txt", new
+	 * Factory() { public Manageable create() { return new RcmdItem(); } }); }
+	 */
+
+	public void view() {
+		mgr.readAll("recommend.txt", new Factory() {
+			public Manageable create() {
+				return new RcmdItem();
+			}
+		});
+		setLayout(Gbag);
+		//JScrollPane scroll = new JScrollPane(this);
+		//scroll.setBounds(0, 0, 160, 160);
+		//create_form(new JLabel(), 0, 0, 0, 0);
+
+		for (Manageable m : mgr.mList) {
+			RcmdItem r = (RcmdItem) m;
+			if(recMatch(r)) {
+				RcmdItemModel md = new RcmdItemModel(r);
+				//add(md);
+				this.create_form(md, 0, count++*30, 30, 10);
+			}
+			
+		}
+	}
+
+	public void create_form(Component cmpt, int x, int y, int w, int h) {
+
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.gridwidth = w;
+		gbc.gridheight = h;
+		this.Gbag.setConstraints(cmpt, gbc);
+		add(cmpt);
+		updateUI();
+
+	}
+	
+public boolean recMatch(RcmdItem r) {
+		
+		if (Integer.parseInt(aA) >= 
+				(Integer.parseInt(cA) / r.month) + (Integer.parseInt(cA) / 1000)) {
+			if ((r.saving).equals("적금")) {
+				return true;
+			}
+			else return false;
+		}
+		else {
+			if ((r.saving).equals("예금")) {
+				return true;
+			}
+			else return false;
+		}
+		
+	}
+
+
+}
---
+++ src/recommend/Recommend.java
@@ -0,0 +1,135 @@
+package recommend;
+
+import javax.swing.JPanel;
+
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import account.Account;
+import account.AccountMgr;
+import mgr.Manageable;
+import table.TableSelectionDemo;
+
+import java.awt.BorderLayout;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import table.MyFrame;
+
+public class Recommend extends JPanel {
+	ArrayList<Account> aList = new ArrayList<>();
+	JTextField kwdTextField1 = new JTextField();
+	JTextField kwdTextField2 = new JTextField();
+	static String kwd1;
+	static String kwd2;
+	
+	String title;
+	
+	
+
+	void run() {
+		readAll("account.txt");
+	}
+
+	void readAll(String filename) {
+		Scanner filein = openFile(filename);
+		Account ac = null;
+		while (filein.hasNext()) {
+			ac = new Account();
+			ac.read(filein);
+			aList.add(ac);
+		}
+	}
+
+	Scanner openFile(String filename) {
+		Scanner filein = null;
+		try {
+			filein = new Scanner(new File(filename));
+		} catch (Exception e) {
+			System.out.printf("파일 오픈 실패: %s\n", filename);
+			System.exit(0);
+		}
+		return filein;
+	}
+
+	/**
+	 * 
+	 * Create the panel.
+	 * 
+	 * @return
+	 * 
+	 */
+
+	public Recommend() {
+		RecommendView();
+	}
+	
+	public Recommend(String title) {
+		this.title = title;
+	}
+	
+	public String getkwd1() {
+		return kwdTextField1.getText();
+	}
+	
+	public String getkwd2() {
+		return kwdTextField2.getText();
+	}
+
+	public void RecommendView() {
+
+		TableSelectionDemo tableDemo = new TableSelectionDemo();
+		setLayout(null);
+		JLabel userLabel = new JLabel("보유현금");
+		userLabel.setBounds(80, 190, 80, 25);
+		add(userLabel);
+
+		JLabel passLabel = new JLabel("저축가능액");
+		passLabel.setBounds(70, 240, 80, 25);
+		add(passLabel);
+		kwdTextField1.setText(null);
+		kwdTextField2.setText(null);
+		kwdTextField1.setBounds(160, 180, 240, 40);
+		kwdTextField2.setBounds(160, 230, 240, 40);
+		add(kwdTextField1);
+		add(kwdTextField2);
+		JButton recommend = new JButton("상품 추천");
+		recommend.setBounds(430, 190, 90, 70);
+		add(recommend);
+		//JFrame jf = new JFrame();
+		//JDialog rd = new RcmdItemDialog(jf, "rec");
+		
+		
+		
+		//run();
+
+		recommend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getActionCommand().equals("상품 추천")) {
+					kwd1 = kwdTextField1.getText();
+					kwd2 = kwdTextField2.getText();
+					tableDemo.showRecommend();
+				}
+				/*if (e.getActionCommand().equals("상품 추천")) {
+					rim.view();
+				}*/
+			}
+		});
+	}
+	
+}
